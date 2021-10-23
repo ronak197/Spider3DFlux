92e77f095d0ae4df3b30efb4c58f948b05112ac5
Biton's Mac <idanbit80@gmail.com>
2021-10-23 15:06:57 +0300

    V1.16.1 (cleaner) My code Updates: Sipping methods dialog improvements V Main.Sub.BugFixes
    - icredit clean req on dart_tests.dart
    - minor bug fix
    
    ✅ Reduce the address form details -s hipping_address.dart (And store data locally)
    ✅ address expansion_Info title Should be dynamic (ec. חבקוק 114, גדרה)
    ⬜ Add a secure cc form (with iCredit validator, can't see full details after submit)
    ✅ Insert "order details" (products list) to row like address details (expansion_info.dart)
    ⬜ Full Validators on final order button On payment_methods.dart line 272
    ⬜ iCredit Api embed - Insert cc token / send cc details / redirect to link an fill cc form with JS (cc = credit card)
    ⬜ Better Stack loading while PaymentMethods reload On review_screen.dart line 344, 368
    
      ✅
      ❌
      Ⓜ️ on work Mode
      ⬜ need to do

--- lib/dart_tests.dart
+++ lib/dart_tests.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
-void main() async {
+// Future<String> iCreditGetUrl(
+Future<String> iCreditGetUrl(
+    {buyer_name, city, street, email, phone, total_price}) async {
   var url =
       'https://testicredit.rivhit.co.il/API/PaymentPageRequest.svc/GetUrl';
 
@@ -10,21 +12,24 @@ void main() async {
     'Items': [
       {
         'Id': 1,
-        'Quantity': 3,
-        'UnitPrice': 55.9,
-        'Description': 'שרשרת פנינים '
+        'Quantity': 1,
+        'UnitPrice': total_price,
+        'Description': 'תשלום מאובטח ביישומון Spider3D'
       }
     ],
-    'RedirectURL': 'http://www.rivhit.co.il',
-    'FailRedirectURL': 'String content',
+    // 'FailRedirectURL': 'String content',
+    // 'Order': 'Woo order number?',
+
+    'CustomerFirstName': buyer_name,
+    // 'CustomerLastName': 'String content',
+    'City': city,
+    'Address': street,
+    'EmailAddress': email,
+    'PhoneNumber': phone,
+
     'MaxPayments': 12,
-    'Order': 'String content',
-    'EmailAddress': 'String content',
-    'CustomerLastName': 'String content',
-    'CustomerFirstName': 'String content',
-    'Address': 'String content',
-    'City': 'String content',
-    'PhoneNumber': 'String content',
+    'RedirectURL':
+        'https://www.spider3d.co.il/%D7%AA%D7%95%D7%93%D7%94/', // spider3d.co.il/תודה
     'DisplayPayPalButton': 'false',
     'CreateToken': 'true'
   };
@@ -33,6 +38,18 @@ void main() async {
   final resp = await dio.post(url, data: req);
 
   // print(resp.data);
-  print(resp.data['PrivateSaleToken']);
-  print(resp.data['URL']);
+  // print(resp.data['PrivateSaleToken']);
+  // print(resp.data['URL']);
+  return resp.data['URL'].toString();
+}
+
+void main() async {
+  var url = await iCreditGetUrl(
+      buyer_name: 'IDAN TEST',
+      city: 'Gedera test',
+      street: 'Hedera',
+      email: 'idan@test.cocom',
+      phone: '0543232761',
+      total_price: 301);
+  print(url);
 }

9b44bf28863db8a33420274586acdcbbc0f8752c
Idan054 <idanbit80@gmail.com>
2021-12-27 17:20:01 +0200

    V12.1 - Send Email [Beta] on dart_tests.dart
    ☐ Auto send mail when variations bug up
    [Beta] :
    + Working mail sender on dart_tests.dart
    + Code base on product_variant_mixin.dart line 597
    (Need to mix them both)
    
    V12.0 Fix Variations Bug (By Hindi Dev)
    -----------------------
    + variations bug base fix on product_variant_mixin.dart (line 508)
    Full details of how to fix common variations bugs on this doc:
    https://docs.google.com/document/d/1SIgqW5HANA_8EZAWANB49MRgRf-DPrXJja_W46fYnsc/edit?usp=sharing
    
    -----------------------
      ☐ Loading until variations up
      ☐ Spider style loading
      ☐ Add thingi token on settings screen
      ☐ (Complete common variations bugs doc)

--- lib/dart_tests.dart
+++ lib/dart_tests.dart
@@ -1,21 +1,52 @@
-import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart'; //For creating the SMTP Server
 
-// Future<String> iCreditGetUrl(
-Future<String> iCreditGetUrl() async {
-  var url =
-      'https://testicredit.rivhit.co.il/API/PaymentPageRequest.svc/GetUrl';
+main() async {
+  // Before use, Enable Less secure App Access for your sccount here: https://myaccount.google.com/lesssecureapps
+  String username = 'idanbit80@gmail.com'; //Your Email;
+  String password = 'Idan@325245'; //Your Email's password;
 
-  var dio = Dio();
-  final resp = await dio.get(url);
+  final smtpServer = gmail(username, password);
+  // final smtpServer = gmailRelaySaslXoauth2(username, password);
+  // final smtpServer = gmailSaslXoauth2(username, password);
+  // Creating the Gmail server
 
-  // print(resp.data);
-  // print(resp.data['PrivateSaleToken']);
-  // print(resp.data['URL']);
-  return resp.data['URL'].toString();
-}
+  // Create our email message.
+  final message = Message()
+    ..from = Address(username)
+    // ..recipients.add('dest@example.com') //recipent email
+    ..ccRecipients.addAll(
+        ['spider3d@info.co.il', 'theblackhero2@gmail.com']) //cc Recipents emails
+    // ..bccRecipients.add(
+    //     Address('hiddenRecipients@example.com')) //bcc Recipents emails
+    ..subject = '${DateTime.now().day}עדכון מיישמון Spider3D 🕷📱️ - פרטי שגיאה (ותיקון) למוצר ווריאציות - ' //subject of the email
+    ..text = '''
+    היי , זוהי הודעה שהוגדרה להשלח אוטומטית במידה ולקוח נתקל בתקלה בעת בחירת ווריאצייה או הוספה לסל.
 
-void main() async {
-  var url = await iCreditGetUrl();
-  print(url);
-}
+    * להלן פרטי המוצר *
+    =================
+    שם המוצר - pname
+    ת.ז המוצר - pid
+    קישור למוצר - link
+    
+    שימו לב! מומלץ להשתמש במדריך "פתרון בעיות נפוצות" על מנת לתקן את המוצר, למדריך:
+    https://docs.google.com/document/d/1SIgqW5HANA_8EZAWANB49MRgRf-DPrXJja_W46fYnsc/edit?usp=sharing
+    
+    * פרטי מלאים על התקלה *
+    ======================
+    זמן התקלה - ${DateTime.now()}
+    תיאור התקלה שהתקבל - errdetails
+    ----------------------------------
+    רצף השגיאות שהתקבל - errTrace
+    '''; //body of the email
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print(
+        'Message sent: ' + sendReport.toString()); //print if the email is sent
+  } on MailerException catch (e) {
+    print(
+        'Message not sent. \n' + e.toString()); //print if the email is not sent
+    // e.toString() will show why the email is not sending
+  }
+} 

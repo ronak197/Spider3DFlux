b2629db2ece6a51ba49d46b6ba43c09af37d710a
Idan054 <idanbit80@gmail.com>
2022-02-10 00:12:24 +0200

    V16.00.0 - delivery_formV3.dart - AutoComplete + Auto complete when edit based official CartModel (Provider)
    V15.03.0 - checkout_screenV3.dart - Static
    + Just a very organize and clean delivery_formV3.dart file
    
    V15.02.0 - checkout_screenV3.dart - Static
    + Connect delivery_screenV3.dart to cards
    
    V15.01.0 - checkout_screenV3.dart - Static
    + checkout_screenV3 added to route_list.dart so now its called when click the Cart button
    + A list of todos in checkout_screenV3.dart
    + performance improvements and bug fixes (: LOL
    
    V15.00.0 - checkout_screenV3.dart - Static
    + After tons of bugs on the checkout screen, its time for makeover!
    
    V14.00.2 - Icredit Fix III
    + Ios CURRENT_PROJECT_VERSION Update (9) on project.pbxproj
    
    V14.0.1 - Icredit Fix II
    + Icredit Fix II
    
    V14.0.0 - Product in cart update (review_screen.dart)
    + ...getProducts() Replaced by ...createShoppingCartRows() : Line 257
    So now user can change quantity & remove items from checkout page.
    
    V13.11.0 - LoginScreen:Cart Bug Fix
    + Change the checking method if user to method based live data. (Needed for apple cuz guest mode availble but not in cart)
    
    V13.10.1 - Coupons loading
    + loading added while searching coupon -
    Based isFetching Bool
    
    V13.10.0 - Coupons fix + summarize
    + Coupon bug fixed
    - ICredit bug fixed (Token Replaced)
    
    V13.9.10 - Login if guest & GreenButton...
    + Green Button is now not float and always show to fix bugs.
    + The Login screen shows if ur guest
    
    V13.9.8 - Improved onboard_screen.dart
    + To fix IOS Release denied, a "Skip login" button has been added (To both OS)
    
    V13.9.7 - [Fix] Checkout button double tap
    + Green Checkout button fixed
    + Fix Checkout button double tap by set Floating cart (bottom left on product page) - product_bottom_sheet.dart - line 278
    
    V13.9.5 - [Fix thingi_screen.dart + Reads Reduce]
    + _check_thingiToken() ( & set_thingiToken() if needed) on every api call
    + Reduce the reads from about 450, can be reduced to ~5 (Limit is 50K)
    + Cleaner II version of set_thingiToken()

--- lib/screens/checkout/checkoutV3/widgets/delivery_formV3.dart
+++ lib/screens/checkout/checkoutV3/widgets/delivery_formV3.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fstore/common/theme/colors.dart';
 import 'package:fstore/models/cart/cart_base.dart';
 import 'package:fstore/models/entities/address.dart';
+import 'package:fstore/models/user_model.dart';
 import 'package:fstore/screens/checkout/widgets/my_creditcard_address.dart';
 import 'package:provider/provider.dart';
 import '../../../../generated/l10n.dart';
@@ -13,11 +15,11 @@ class DeliveryFormV3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _formKey = GlobalKey<FormState>();
-    final _nameController = TextEditingController();
-    final _cityController = TextEditingController();
-    final _streetController = TextEditingController();
-    final _phoneController = TextEditingController();
-    final _emailController = TextEditingController();
+    var _nameController = TextEditingController();
+    var _cityController = TextEditingController();
+    var _streetController = TextEditingController();
+    var _phoneController = TextEditingController();
+    var _emailController = TextEditingController();
 
     final _cityNode = FocusNode();
     final _streetNode = FocusNode();
@@ -26,131 +28,159 @@ class DeliveryFormV3 extends StatelessWidget {
 
     // Address? address; // Does it necessary?
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(5),
-      child: Container(
-        // color: Color(0xfff1f1f1),
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Form(
-              // key: _formKey, //   final _formKey = GlobalKey<FormState>();
-              child: AutofillGroup( // Do we really need those?
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: greyTxtDeco(labelText: 'שם לחשבונית'),
-                        controller: _nameController,
-                        autofillHints: [AutofillHints.givenName],
-                        textInputAction: TextInputAction.next,
-                        autofocus: true,
-                        onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_cityNode),
-                        validator: (val) =>
-                            val!.isEmpty ? S.of(context).firstNameIsRequired : null,
-                      ),
-                      Row(
-                        children: [
-                          Flexible(
-                            flex: 7,
-                            child: TextFormField(
-                              decoration: greyTxtDeco(labelText: 'עיר'),
-                              controller: _cityController,
-                              autofillHints: [AutofillHints.addressCity],
-                              focusNode: _cityNode,
-                              textInputAction: TextInputAction.next,
-                              onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_streetNode),
-                              validator: (val) =>
-                                 val!.isEmpty ? S.of(context).cityIsRequired : null,
-                            ),
+    return Consumer<CartModel>(
+      builder: (context, model, child) {
+        // print('UserModel Details:');
+        // print('user loggedIn? ${model.user?.loggedIn}');
+        // print('user name: ${model.user?.name}');
+        // print('user email: ${model.user?.email}');
+
+        if (kDebugMode) {
+          print('CartModel Details:');
+          print('cart loggedIn? ${model.user?.loggedIn}');
+          print('cart user ${model.user}');
+          print('cart address ${model.address}');
+          print('cart checkout ${model.checkout}');
+        }
+
+        if (model.user?.username != null) _nameController.text = '${model.user?.username}';
+        if (model.address?.city != null) _cityController.text = '${model.address?.city}';
+        if (model.address?.street != null) _streetController.text = '${model.address?.street}';
+        if (model.address?.phoneNumber != null) {
+          _phoneController.text = '${model.address?.phoneNumber}';}
+        if (model.address?.email != null) _emailController.text = '${model.address?.email}';
+
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Container(
+            // color: Color(0xfff1f1f1),
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Form(
+                  // key: _formKey, //   final _formKey = GlobalKey<FormState>();
+                  child: AutofillGroup( // Do we really need those?
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: greyTxtDeco(labelText: 'שם לחשבונית'),
+                            controller: _nameController,
+                            autofillHints: [AutofillHints.givenName],
+                            textInputAction: TextInputAction.next,
+                            autofocus: model.user?.username != null ? false : true, // focus if empty
+                            onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_cityNode),
+                            validator: (val) =>
+                                val!.isEmpty ? S.of(context).firstNameIsRequired : null,
                           ),
-                          const Spacer(
-                            flex: 1,
-                          ),
-                          Flexible(
-                            flex: 11,
-                            child: TextFormField(
-                                decoration: greyTxtDeco(labelText: 'רחוב, מס׳ בית'),
-                                controller: _streetController,
-                                autofillHints: [
-                                  AutofillHints.fullStreetAddress
-                                ],
-                                focusNode: _streetNode,
-                                textInputAction: TextInputAction.next,
-                                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_phoneNode),
-                                validator: (val) =>
-                                   val!.isEmpty ? S.of(context).streetIsRequired : null,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Flexible(
-                            flex: 9,
-                            child: TextFormField(
-                                decoration: greyTxtDeco(labelText: 'טלפון'),
-                                controller: _phoneController,
-                                autofillHints: [AutofillHints.telephoneNumber],
-                                focusNode: _phoneNode,
-                                keyboardType: TextInputType.number,
-                                textInputAction: TextInputAction.next,
-                                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_emailNode),
-                                validator: (val) {
-                                    // print('val.length');
-                                    // print(val!.length);
-                                    return val!.length != 10 ? 'הזן טלפון תקין' : null;
-                                    },
+                          Row(
+                            children: [
+                              Flexible(
+                                flex: 7,
+                                child: TextFormField(
+                                  decoration: greyTxtDeco(labelText: 'עיר'),
+                                  controller: _cityController,
+                                  autofillHints: [AutofillHints.addressCity],
+                                  focusNode: _cityNode,
+                                  textInputAction: TextInputAction.next,
+                                  autofocus:
+                                  model.user?.username != null
+                                  && model.address?.city == null
+                                      ? true : false, // focus if name already filled
+                                  onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_streetNode),
+                                  validator: (val) =>
+                                     val!.isEmpty ? S.of(context).cityIsRequired : null,
                                 ),
+                              ),
+                              const Spacer(
+                                flex: 1,
+                              ),
+                              Flexible(
+                                flex: 11,
+                                child: TextFormField(
+                                    decoration: greyTxtDeco(labelText: 'רחוב, מס׳ בית'),
+                                    controller: _streetController,
+                                    autofillHints: [
+                                      AutofillHints.fullStreetAddress
+                                    ],
+                                    focusNode: _streetNode,
+                                    textInputAction: TextInputAction.next,
+                                    onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_phoneNode),
+                                    validator: (val) =>
+                                       val!.isEmpty ? S.of(context).streetIsRequired : null,
+                                ),
+                              ),
+                            ],
                           ),
-                          const Spacer(
-                            flex: 1,
+                          Row(
+                            children: [
+                              Flexible(
+                                flex: 9,
+                                child: TextFormField(
+                                    decoration: greyTxtDeco(labelText: 'טלפון'),
+                                    controller: _phoneController,
+                                    autofillHints: [AutofillHints.telephoneNumber],
+                                    focusNode: _phoneNode,
+                                    keyboardType: TextInputType.number,
+                                    textInputAction: TextInputAction.next,
+                                    onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_emailNode),
+                                    validator: (val) {
+                                        // print('val.length');
+                                        // print(val!.length);
+                                        return val!.length != 10 ? 'הזן טלפון תקין' : null;
+                                        },
+                                    ),
+                              ),
+                              const Spacer(
+                                flex: 1,
+                              ),
+                              Flexible(
+                                flex: 9,
+                                child: TextFormField(
+                                    decoration: greyTxtDeco(labelText: 'אימייל'),
+                                    controller: _emailController,
+                                    autofillHints: [AutofillHints.email],
+                                    focusNode: _emailNode,
+                                    keyboardType: TextInputType.emailAddress,
+                                    textInputAction: TextInputAction.done,
+                                    validator: (val) {
+                                      if (val!.isEmpty) return S.of(context).emailIsRequired;
+                                      return validateEmail(val);
+                                    },
+                                    onFieldSubmitted: (_) async {
+                                      await _handleDoneButton(context);
+                                    },),
+                              ),
+                            ],
                           ),
-                          Flexible(
-                            flex: 9,
-                            child: TextFormField(
-                                decoration: greyTxtDeco(labelText: 'אימייל'),
-                                controller: _emailController,
-                                autofillHints: [AutofillHints.email],
-                                focusNode: _emailNode,
-                                keyboardType: TextInputType.emailAddress,
-                                textInputAction: TextInputAction.done,
-                                validator: (val) {
-                                  if (val!.isEmpty) return S.of(context).emailIsRequired;
-                                  return validateEmail(val);
-                                },
-                                onFieldSubmitted: (_) async {
+                          Center(
+                            child: ButtonTheme(
+                              height: 45,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 0.0,
+                                  onPrimary: Colors.white,
+                                  primary: Theme
+                                      .of(context)
+                                      .primaryColor,
+                                ),
+                                onPressed: () async {
                                   await _handleDoneButton(context);
-                                },),
-                          ),
-                        ],
-                      ),
-                      Center(
-                        child: ButtonTheme(
-                          height: 45,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              elevation: 0.0,
-                              onPrimary: Colors.white,
-                              primary: Theme
-                                  .of(context)
-                                  .primaryColor,
+                                },
+                                child: const Text('עדכן',
+                                    style: TextStyle(fontSize: 14)),
+                              ),
                             ),
-                            onPressed: () async {
-                              await _handleDoneButton(context);
-                            },
-                            child: const Text('עדכן',
-                                style: TextStyle(fontSize: 14)),
-                          ),
-                        ),
-                      )
-                    ]),
-              ),
+                          )
+                        ]),
+                  ),
+                ),
+                // Services().widget.renderShippingMethods(context, onBack: () {}, onNext: () {}),
+              ],
             ),
-            // Services().widget.renderShippingMethods(context, onBack: () {}, onNext: () {}),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
   String? validateEmail(String value) {
